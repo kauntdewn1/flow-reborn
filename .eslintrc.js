aa9be89dcce5578c971d58d11d84f50472691449
MyrnaClyde <114366115+kauntdewn1@users.noreply.github.com>
2025-05-03 18:15:31 -0300

    fix: update dependencies and simplify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: ['next/core-web-vitals'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn'
+    'react/prop-types': 'off'
   }
 } 
